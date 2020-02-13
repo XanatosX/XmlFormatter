400bbbb8aee07d321d574c0561f09fc7723fa74e
XanatosX <simonaberle@gmx.de>
2020-02-13 22:58:52 +0100

    Adding new setting to the project

--- XmlFormatter/src/Windows/Settings.Designer.cs
+++ XmlFormatter/src/Windows/Settings.Designer.cs
@@ -31,13 +31,17 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CB_CheckUpdatesOnStartup = new System.Windows.Forms.CheckBox();
             this.CB_AskBeforeClose = new System.Windows.Forms.CheckBox();
             this.CB_MinimizeToTray = new System.Windows.Forms.CheckBox();
             this.B_SaveAndClose = new System.Windows.Forms.Button();
             this.B_Cancel = new System.Windows.Forms.Button();
-            this.TT_Components = new System.Windows.Forms.ToolTip(this.components);
-            this.CB_CheckUpdatesOnStartup = new System.Windows.Forms.CheckBox();
+            this.MI_SettingsMenu = new System.Windows.Forms.MenuStrip();
+            this.exportSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.groupBox1.SuspendLayout();
+            this.MI_SettingsMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -45,12 +49,22 @@
             this.groupBox1.Controls.Add(this.CB_CheckUpdatesOnStartup);
             this.groupBox1.Controls.Add(this.CB_AskBeforeClose);
             this.groupBox1.Controls.Add(this.CB_MinimizeToTray);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(239, 91);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Application settings";
+            // 
+            // CB_CheckUpdatesOnStartup
+            // 
+            this.CB_CheckUpdatesOnStartup.AutoSize = true;
+            this.CB_CheckUpdatesOnStartup.Location = new System.Drawing.Point(6, 65);
+            this.CB_CheckUpdatesOnStartup.Name = "CB_CheckUpdatesOnStartup";
+            this.CB_CheckUpdatesOnStartup.Size = new System.Drawing.Size(163, 17);
+            this.CB_CheckUpdatesOnStartup.TabIndex = 2;
+            this.CB_CheckUpdatesOnStartup.Text = "Check for updates on startup";
+            this.CB_CheckUpdatesOnStartup.UseVisualStyleBackColor = true;
             // 
             // CB_AskBeforeClose
             // 
@@ -74,7 +88,7 @@
             // 
             // B_SaveAndClose
             // 
-            this.B_SaveAndClose.Location = new System.Drawing.Point(12, 109);
+            this.B_SaveAndClose.Location = new System.Drawing.Point(12, 124);
             this.B_SaveAndClose.Name = "B_SaveAndClose";
             this.B_SaveAndClose.Size = new System.Drawing.Size(104, 23);
             this.B_SaveAndClose.TabIndex = 1;
@@ -84,7 +98,7 @@
             // 
             // B_Cancel
             // 
-            this.B_Cancel.Location = new System.Drawing.Point(176, 109);
+            this.B_Cancel.Location = new System.Drawing.Point(176, 124);
             this.B_Cancel.Name = "B_Cancel";
             this.B_Cancel.Size = new System.Drawing.Size(75, 23);
             this.B_Cancel.TabIndex = 2;
@@ -92,33 +106,52 @@
             this.B_Cancel.UseVisualStyleBackColor = true;
             this.B_Cancel.Click += new System.EventHandler(this.B_Cancel_Click);
             // 
-            // CB_CheckUpdatesOnStartup
+            // MI_SettingsMenu
             // 
-            this.CB_CheckUpdatesOnStartup.AutoSize = true;
-            this.CB_CheckUpdatesOnStartup.Location = new System.Drawing.Point(6, 65);
-            this.CB_CheckUpdatesOnStartup.Name = "CB_CheckUpdatesOnStartup";
-            this.CB_CheckUpdatesOnStartup.Size = new System.Drawing.Size(163, 17);
-            this.CB_CheckUpdatesOnStartup.TabIndex = 2;
-            this.CB_CheckUpdatesOnStartup.Text = "Check for updates on startup";
-            this.CB_CheckUpdatesOnStartup.UseVisualStyleBackColor = true;
+            this.MI_SettingsMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportSettingsToolStripMenuItem,
+            this.importSettingsToolStripMenuItem});
+            this.MI_SettingsMenu.Location = new System.Drawing.Point(0, 0);
+            this.MI_SettingsMenu.Name = "MI_SettingsMenu";
+            this.MI_SettingsMenu.Size = new System.Drawing.Size(263, 24);
+            this.MI_SettingsMenu.TabIndex = 3;
+            this.MI_SettingsMenu.Text = "menuStrip1";
+            // 
+            // exportSettingsToolStripMenuItem
+            // 
+            this.exportSettingsToolStripMenuItem.Name = "exportSettingsToolStripMenuItem";
+            this.exportSettingsToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
+            this.exportSettingsToolStripMenuItem.Text = "Export settings";
+            this.exportSettingsToolStripMenuItem.Click += new System.EventHandler(this.exportSettingsToolStripMenuItem_Click);
+            // 
+            // importSettingsToolStripMenuItem
+            // 
+            this.importSettingsToolStripMenuItem.Name = "importSettingsToolStripMenuItem";
+            this.importSettingsToolStripMenuItem.Size = new System.Drawing.Size(99, 20);
+            this.importSettingsToolStripMenuItem.Text = "Import settings";
             // 
             // Settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(263, 141);
+            this.ClientSize = new System.Drawing.Size(263, 159);
             this.Controls.Add(this.B_Cancel);
             this.Controls.Add(this.B_SaveAndClose);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.MI_SettingsMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.MI_SettingsMenu;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Settings";
             this.Text = "Settings";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.MI_SettingsMenu.ResumeLayout(false);
+            this.MI_SettingsMenu.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -129,7 +162,10 @@
         private System.Windows.Forms.CheckBox CB_AskBeforeClose;
         private System.Windows.Forms.Button B_SaveAndClose;
         private System.Windows.Forms.Button B_Cancel;
-        private System.Windows.Forms.ToolTip TT_Components;
         private System.Windows.Forms.CheckBox CB_CheckUpdatesOnStartup;
+        private System.Windows.Forms.MenuStrip MI_SettingsMenu;
+        private System.Windows.Forms.ToolStripMenuItem exportSettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importSettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
