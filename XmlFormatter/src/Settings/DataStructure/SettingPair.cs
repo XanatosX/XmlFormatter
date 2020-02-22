438ee87c104eeb65b3683aadf97db08264e01af0
XanatosX <simonaberle@gmx.de>
2020-02-22 20:16:26 +0100

    Adding summary blocks
    Removed unsued namespaces
    Small refactoring
    Removed empty lines

--- XmlFormatter/src/Settings/DataStructure/SettingPair.cs
+++ XmlFormatter/src/Settings/DataStructure/SettingPair.cs
@@ -1,34 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using XmlFormatter.src.Interfaces.Settings.DataStructure;
 
 namespace XmlFormatter.src.Settings.DataStructure
 {
+    /// <inheritdoc/>
     class SettingPair : ISettingPair
     {
+        /// <summary>
+        /// The key/name of this setting pair
+        /// </summary>
         private readonly string name;
+
+        /// <inheritdoc/>
         public string Name => name;
 
+        /// <summary>
+        /// The type of the value stored in this key value pair
+        /// </summary>
         private Type type;
+
+        /// <inheritdoc/>
         public Type Type => type;
 
+        /// <summary>
+        /// The value object of this setting pair
+        /// </summary>
         private object value;
+
+        /// <inheritdoc/>
         public object Value => value;
 
+        /// <summary>
+        /// Create a new setting pair
+        /// </summary>
+        /// <param name="name">The key/name of the new setting</param>
         public SettingPair(string name)
         {
             this.name = name;
         }
 
+        /// <inheritdoc/>
         public void SetValue<T>(T value)
         {
             this.value = value;
             type = value.GetType();
         }
 
+        /// <inheritdoc/>
         public T GetValue<T>()
         {
             try
@@ -42,7 +60,6 @@ namespace XmlFormatter.src.Settings.DataStructure
             }
 
             return default(T);
-
         }
     }
 }
--- XmlFormatter/src/Settings/DataStructure/SettingScope.cs
+++ XmlFormatter/src/Settings/DataStructure/SettingScope.cs
@@ -1,39 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using XmlFormatter.src.Interfaces.Settings.DataStructure;
 
 namespace XmlFormatter.src.Settings.DataStructure
 {
+    /// <inheritdoc/>
     class SettingScope : ISettingScope
     {
+        /// <summary>
+        /// The key/name for this scope
+        /// </summary>
         private string name;
+
+        /// <inheritdoc/>
         public string Name => name;
 
+        /// <summary>
+        /// All the sub scopes for this specific scopes
+        /// This is a readonly list
+        /// </summary>
         private readonly List<ISettingScope> subScopes;
 
+        /// <summary>
+        /// All the settings in this scope
+        /// The list is readonly
+        /// </summary>
         private readonly List<ISettingPair> settings;
 
+        /// <summary>
+        /// Create a new instance with empty lists
+        /// </summary>
         public SettingScope()
         {
             settings = new List<ISettingPair>();
             subScopes = new List<ISettingScope>();
         }
 
+        /// <summary>
+        /// Create a new instance and set a name
+        /// </summary>
+        /// <param name="name"></param>
         public SettingScope(string name)
+            : this()
         {
-            settings = new List<ISettingPair>();
-            subScopes = new List<ISettingScope>();
             SetName(name);
         }
 
+        /// <inheritdoc/>
         public void SetName(string name)
         {
             this.name = name;
         }
 
+        /// <inheritdoc/>
         public void AddSetting(ISettingPair setting)
         {
             ISettingPair stored = GetSetting(setting.Name);
@@ -46,6 +64,7 @@ namespace XmlFormatter.src.Settings.DataStructure
             settings.Add(setting);
         }
 
+        /// <inheritdoc/>
         public ISettingPair GetSetting(string name)
         {
             return settings.Find((currentObject) =>
@@ -54,11 +73,13 @@ namespace XmlFormatter.src.Settings.DataStructure
             });
         }
 
+        /// <inheritdoc/>
         public List<ISettingPair> GetSettings()
         {
             return settings;
         }
 
+        /// <inheritdoc/>
         public void AddSubScope(ISettingScope scope)
         {
             subScopes.RemoveAll((currentScope) =>
@@ -68,6 +89,7 @@ namespace XmlFormatter.src.Settings.DataStructure
             subScopes.Add(scope);
         }
 
+        /// <inheritdoc/>
         public ISettingScope GetSubScope(string name)
         {
             return subScopes.Find((currentScope) =>
@@ -76,11 +98,10 @@ namespace XmlFormatter.src.Settings.DataStructure
             });
         }
 
+        /// <inheritdoc/>
         public List<ISettingScope> GetSubScopes()
         {
             return subScopes;
         }
-
-
     }
 }
