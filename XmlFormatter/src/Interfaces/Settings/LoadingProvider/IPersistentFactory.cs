9b6bf25425106a91686360559a3b72aeb2f45369
XanatosX <simonaberle@gmx.de>
2020-02-22 20:01:52 +0100

    Adding summary blocks

--- XmlFormatter/src/Interfaces/Settings/LoadingProvider/IPersistentFactory.cs
+++ XmlFormatter/src/Interfaces/Settings/LoadingProvider/IPersistentFactory.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace XmlFormatter.src.Interfaces.Settings.LoadingProvider
 {
+    /// <summary>
+    /// A factory to get saving and loading provider for the setting manager
+    /// </summary>
     public interface IPersistentFactory
     {
+        /// <summary>
+        /// Create and return the loading prodiver to use
+        /// </summary>
+        /// <returns>A loading provider which can be used to load files</returns>
         ISettingLoadProvider CreateLoader();
 
+        /// <summary>
+        /// Create and return the saving provider to use
+        /// </summary>
+        /// <returns>A saving provider which can be used to save the files</returns>
         ISettingSaveProvider CreateSaver();
     }
 }
--- XmlFormatter/src/Interfaces/Settings/LoadingProvider/ISettingSaveProvider.cs
+++ XmlFormatter/src/Interfaces/Settings/LoadingProvider/ISettingSaveProvider.cs
@@ -7,8 +7,17 @@ using XmlFormatter.src.Settings;
 
 namespace XmlFormatter.src.Interfaces.Settings.LoadingProvider
 {
+    /// <summary>
+    /// This interface is representing a save provider for the settings manager
+    /// </summary>
     public interface ISettingSaveProvider
     {
+        /// <summary>
+        /// Save all the scope which are in the manager
+        /// </summary>
+        /// <param name="settingsManager">The manager to save</param>
+        /// <param name="filePath">The file path to save the data to</param>
+        /// <returns>True if saving was a success</returns>
         bool SaveSettings(ISettingsManager settingsManager, string filePath);
     }
 }
