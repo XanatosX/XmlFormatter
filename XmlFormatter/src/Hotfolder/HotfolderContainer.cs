96e5ec82d229f3a68c67639ca4285bf86339f966
XanatosX <simonaberle@gmx.de>
2020-02-23 09:24:11 +0100

    Adding summary blocks

--- XmlFormatter/src/Hotfolder/HotfolderContainer.cs
+++ XmlFormatter/src/Hotfolder/HotfolderContainer.cs
@@ -11,58 +11,100 @@ namespace XmlFormatter.src.Hotfolder
 {
     class HotfolderContainer : IHotfolder
     {
+        /// <summary>
+        /// The mode to use for formatting
+        /// </summary>
         private ModesEnum mode;
+
+        /// <inheritdoc/>
         public ModesEnum Mode
         {
             get => mode;
             set => mode = value;
         }
 
+        /// <summary>
+        /// The formatter to use
+        /// </summary>
         private IFormatter formatterToUse;
+
+        /// <inheritdoc/>
         public IFormatter FormatterToUse => formatterToUse;
 
         private string watchedFolder;
+
+        /// <inheritdoc/>
         public string WatchedFolder
         {
             get => watchedFolder;
             set => watchedFolder = value;
         }
 
+        /// <summary>
+        /// The filter to use in the watched folder
+        /// </summary>
         private string filter;
+
+        /// <inheritdoc/>
         public string Filter
         {
             get => filter;
             set => filter = value; 
         }
 
+        /// <summary>
+        /// The output folder to save the new file in
+        /// </summary>
         private string outputFolder;
+
+        /// <inheritdoc/>
         public string OutputFolder
         {
             get => outputFolder;
             set => outputFolder = value;
         }
 
+        /// <summary>
+        /// The scheme of the output file
+        /// </summary>
         private string outputFileScheme;
+
+        /// <inheritdoc/>
         public string OutputFileScheme
         {
             get => outputFileScheme;
             set => outputFileScheme = value;
         }
 
+        /// <summary>
+        /// Should we trigger on rename as well
+        /// </summary>
         private bool onRename;
+
+        /// <inheritdoc/>
         public bool OnRename
         {
             get => onRename;
             set => onRename = value;
         }
 
+        /// <summary>
+        /// Should we delete the old file
+        /// </summary>
         private bool removeOld;
+
+        /// <inheritdoc/>
         public bool RemoveOld
         {
             get => removeOld;
             set => removeOld = value;
         }
 
+        /// <summary>
+        /// Create a new instance of the hotfolder configuration
+        /// </summary>
+        /// <param name="formatter">The formatter class to use</param>
+        /// <param name="watchedFolder">The hotfolder to watch</param>
         public HotfolderContainer(IFormatter formatter, string watchedFolder)
         {
             formatterToUse = formatter;
