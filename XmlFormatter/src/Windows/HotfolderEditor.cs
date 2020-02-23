18ed01f3a1fdeb5a00d849a9620bf3e22e2bbf35
XanatosX <simonaberle@gmx.de>
2020-02-23 10:23:10 +0100

    Adding summary blocks
    Removed unused namespaces

--- XmlFormatter/src/Windows/HotfolderEditor.cs
+++ XmlFormatter/src/Windows/HotfolderEditor.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using XmlFormatter.src.Enums;
 using XmlFormatter.src.Hotfolder;
@@ -16,18 +12,45 @@ using XmlFormatter.src.Interfaces.Hotfolder;
 
 namespace XmlFormatter.src.Windows
 {
+    /// <summary>
+    /// This Window is used to create or edit a hotfolder configuration
+    /// </summary>
     public partial class HotfolderEditor : Form
     {
-        private bool editMode;
+        /// <summary>
+        /// Is the editor in the hotfolder edit mode
+        /// </summary>
+        private readonly bool editMode;
 
+        /// <summary>
+        /// Was something saved
+        /// </summary>
         private bool saved;
+
+        /// <summary>
+        /// Readonly access if something was saved
+        /// </summary>
         public bool Saved => saved;
 
+        /// <summary>
+        /// A lookup for the formatters
+        /// </summary>
         private Dictionary<string, Type> formatters;
 
+        /// <summary>
+        /// The current hotfolder configuration
+        /// </summary>
         private IHotfolder hotfolder;
+
+        /// <summary>
+        /// Readonly access to the hotfolder configuration
+        /// </summary>
         public IHotfolder Hotfolder => hotfolder;
 
+        /// <summary>
+        /// Create a new instance of the editor
+        /// </summary>
+        /// <param name="hotfolder">The hotfolder configuration to use for editing</param>
         public HotfolderEditor(IHotfolder hotfolder)
         {
             editMode = hotfolder == null ? false : true;
@@ -40,6 +63,11 @@ namespace XmlFormatter.src.Windows
         }
 
 
+        /// <summary>
+        /// Loading the window
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">Event arguments</param>
         private void HotfolderEditor_Load(object sender, EventArgs e)
         {
             Type type = typeof(IFormatter);
@@ -104,6 +132,11 @@ namespace XmlFormatter.src.Windows
                 }
         }
 
+        /// <summary>
+        /// Folder selector if we want to set a folder to watch
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">Event arguments</param>
         private void B_OpenWatchFolder_Click(object sender, EventArgs e)
         {
             GetFolderPath(TB_WatchedFolder);
@@ -113,11 +146,20 @@ namespace XmlFormatter.src.Windows
             }
         }
 
+        /// <summary>
+        /// Folder selecton if we want to set the folder for output
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">Event arguments</param>
         private void B_OutputFolder_Click(object sender, EventArgs e)
         {
             GetFolderPath(TB_OutputFolder);
         }
 
+        /// <summary>
+        /// Get the folder path from a folder selector
+        /// </summary>
+        /// <param name="targetBox">The text box to write the result into</param>
         private void GetFolderPath(TextBox targetBox)
         {
             FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
@@ -130,6 +172,11 @@ namespace XmlFormatter.src.Windows
             targetBox.Text = folderBrowser.SelectedPath;
         }
 
+        /// <summary>
+        /// Save the current configuration as hotfolder
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">Event arguments</param>
         private void B_Save_Click(object sender, EventArgs e)
         {
             if (!Directory.Exists(TB_WatchedFolder.Text))
@@ -167,6 +214,11 @@ namespace XmlFormatter.src.Windows
             B_Cancel.PerformClick();
         }
 
+        /// <summary>
+        /// Close this window without saving anything
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">Event arguments</param>
         private void B_Cancel_Click(object sender, EventArgs e)
         {
             this.Close();
