d3e7b3e9624eb5a194c7cd90defd3007b908c41c
XanatosX <simonaberle@gmx.de>
2020-02-29 11:14:00 +0100

    Adding summary blocks

--- XmlFormatter/src/Logging/LoggingManager.cs
+++ XmlFormatter/src/Logging/LoggingManager.cs
@@ -1,38 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using XmlFormatter.src.DataContainer.Logging;
 using XmlFormatter.src.Interfaces.Logging;
 
 namespace XmlFormatter.src.Logging
 {
+    /// <summary>
+    /// This class is a simple logging manager
+    /// </summary>
     class LoggingManager : ILoggingManager
     {
+        /// <summary>
+        /// A list with all the loggers in the manager
+        /// </summary>
         private readonly List<ILogger> loggers;
 
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
         public LoggingManager()
         {
             loggers = new List<ILogger>();
         }
 
+        /// <inheritdoc/>
         public bool AddLogger(ILogger logger)
         {
             loggers.Add(logger);
             return true;
         }
 
+        /// <inheritdoc/>
         public bool RemoveLogger(ILogger logger)
         {
             return loggers.Remove(logger);
         }
 
+        /// <inheritdoc/>
         public void RemoveLoggers()
         {
             loggers.Clear();
         }
 
+        /// <inheritdoc/>
         public bool LogMessage(LoggingMessage message)
         {
             bool status = true;
