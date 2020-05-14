50c8b4eec2a06b76666d67d07a15794fd2cf05d8
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-05-14 22:06:06 +0200

    Adding missing summary blocks
    Correcting fields

--- PluginFramework/src/Update/BaseUpdate.cs
+++ PluginFramework/src/Update/BaseUpdate.cs
@@ -9,13 +9,23 @@ using System.Windows.Forms;
 
 namespace PluginFramework.src.Update
 {
+    /// <summary>
+    /// Basic class for a new update plugin
+    /// </summary>
     public abstract class BaseUpdate : IUpdateStrategy
     {
+        /// <summary>
+        /// Settings of the update plugin
+        /// </summary>
         public PluginSettings Settings { get; private set; }
 
         /// <inheritdoc/>
         public PluginInformation Information { get; }
 
+        /// <summary>
+        /// Create a new intsance of this base plugin calss
+        /// </summary>
+        /// <param name="information">The plugin information to use</param>
         protected BaseUpdate(PluginInformation information)
         {
             Information = information;
