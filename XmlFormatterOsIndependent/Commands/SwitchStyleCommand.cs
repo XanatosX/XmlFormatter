933150099e91ee3c563df8cd044aebf8ee71c24b
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-05-31 21:54:18 +0200

    Preparing for dark mode

---
+++ XmlFormatterOsIndependent/Commands/SwitchStyleCommand.cs
@@ -0,0 +1,46 @@
+﻿using Avalonia.Markup.Xaml.Styling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using XmlFormatterOsIndependent.DataSets;
+using XmlFormatterOsIndependent.Enums;
+
+namespace XmlFormatterOsIndependent.Commands
+{
+    class SwitchStyleCommand : ICommand
+    {
+        private readonly StyleInclude light;
+        private readonly StyleInclude dark;
+
+        public SwitchStyleCommand()
+        {
+            light = new StyleInclude(new Uri("resm:Styles?assembly=ControlCatalog"))
+            {
+                Source = new Uri("resm:Avalonia.Themes.Default.Accents.BaseLight.xaml?assembly=Avalonia.Themes.Default")
+            };
+
+            dark = new StyleInclude(new Uri("resm:Styles?assembly=ControlCatalog"))
+            {
+                Source = new Uri("resm:Avalonia.Themes.Default.Accents.BaseDark.xaml?assembly=Avalonia.Themes.Default")
+            };
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return parameter is ThemeSwitchData;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is ThemeSwitchData data)
+            {
+                StyleInclude styleToUse = data.Theme == ThemeEnum.Dark ? dark : light;
+                data.View.GetWindow().Styles.Add(styleToUse);
+
+            }
+        }
+    }
+}
---
+++ XmlFormatterOsIndependent/Enums/ThemeEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XmlFormatterOsIndependent.Enums
+{
+    internal enum ThemeEnum
+    {
+        Dark,
+        Light
+    }
+}
