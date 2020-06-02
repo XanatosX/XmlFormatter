343eddce2d54cb3823a08c80797956f8149b2c97
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-06-02 19:28:25 +0200

    Adding new class for updating

---
+++ XmlFormatterOsIndependent/Commands/ExecuteUpdateStrategyCommand.cs
@@ -0,0 +1,44 @@
+﻿using PluginFramework.Interfaces.PluginTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XmlFormatterOsIndependent.DataSets;
+
+namespace XmlFormatterOsIndependent.Commands
+{
+    internal class ExecuteUpdateStrategyCommand : BaseDataCommand
+    {
+        public override Task AsyncExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return parameter is UpdateApplicationData;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if (parameter is UpdateApplicationData data)
+            {
+                if (data.Strategy == null)
+                {
+                    return;
+                }
+                data.Strategy.Update(data.VersionCompare);
+            }
+        }
+
+        public override T GetData<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsExecuted()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
