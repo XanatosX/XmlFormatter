0027c4a3c3166eafe27dae146189c992c3aa0923
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-06-03 18:37:24 +0200

    Adding missing summary blocks

--- XmlFormatterOsIndependent/Commands/BaseDataCommand.cs
+++ XmlFormatterOsIndependent/Commands/BaseDataCommand.cs
@@ -3,36 +3,53 @@ using System.Threading.Tasks;
 
 namespace XmlFormatterOsIndependent.Commands
 {
+    /// <summary>
+    /// This class is a base class for any data driven command
+    /// </summary>
     public abstract class BaseDataCommand : IDataCommand
     {
+        /// <inheritdoc/>
         public event EventHandler CanExecuteChanged;
+
+        /// <inheritdoc/>
         public event EventHandler Executed;
 
-        public virtual void CanExecute()
+        /// <inheritdoc/>
+        public virtual bool CanExecute()
         {
-            CanExecute(null);
+            return CanExecute(null);
         }
 
+        /// <inheritdoc/>
         public virtual void Execute()
         {
             Execute(null);
         }
 
+        /// <inheritdoc/>
         public abstract bool CanExecute(object parameter);
 
+        /// <inheritdoc/>
         public abstract void Execute(object parameter);
 
+        /// <inheritdoc/>
         public abstract T GetData<T>();
 
+        /// <inheritdoc/>
         public abstract bool IsExecuted();
 
+        /// <inheritdoc/>
         public Task AsyncExecute()
         {
             return AsyncExecute(null);
         }
 
+        /// <inheritdoc/>
         public abstract Task AsyncExecute(object parameter);
 
+        /// <summary>
+        /// Trigger the event that the execution was done
+        /// </summary>
         protected void ExecutionDone()
         {
             EventHandler handler = Executed;
