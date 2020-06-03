fc0539c87e3ccf07f6f1ce8967f863631c5c254c
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-06-03 19:39:26 +0200

    Adding summary block

--- XmlFormatterOsIndependent/Update/LocalVersionRecieverStrategy.cs
+++ XmlFormatterOsIndependent/Update/LocalVersionRecieverStrategy.cs
@@ -7,20 +7,29 @@ using XmlFormatterModel.Update.Strategies;
 
 namespace XmlFormatterOsIndependent.Update
 {
+    /// <summary>
+    /// Strategy to use to get the local version
+    /// </summary>
     internal class LocalVersionRecieverStrategy : IVersionRecieverStrategy
     {
+        /// <summary>
+        /// Was there any error in recieving the version
+        /// </summary>
         public event EventHandler<BaseEventArgs> Error;
 
+        /// <inheritdoc/>
         public async Task<IRelease> GetLatestRelease()
         {
             return default;
         }
 
+        /// <inheritdoc/>
         public async Task<List<IRelease>> GetReleases()
         {
             return default;
         }
 
+        /// <inheritdoc/>
         public async Task<Version> GetVersion(IVersionConvertStrategy convertStrategy)
         {
             return new Version(0, 0);
