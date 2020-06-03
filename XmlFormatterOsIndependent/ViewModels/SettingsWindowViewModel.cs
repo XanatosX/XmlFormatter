2247ec5cd32f44ca65ea3fddef944e0dc096f827
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-06-03 19:54:21 +0200

    Adding summary blocks

--- XmlFormatterOsIndependent/ViewModels/SettingsWindowViewModel.cs
+++ XmlFormatterOsIndependent/ViewModels/SettingsWindowViewModel.cs
@@ -2,7 +2,6 @@
 using PluginFramework.Interfaces.Manager;
 using PluginFramework.Interfaces.PluginTypes;
 using ReactiveUI;
-using System;
 using System.Collections.Generic;
 using System.Windows.Input;
 using XmlFormatterModel.Setting;
@@ -12,46 +11,92 @@ using XmlFormatterOsIndependent.Enums;
 
 namespace XmlFormatterOsIndependent.ViewModels
 {
+    /// <summary>
+    /// View model class for the settings window
+    /// </summary>
     internal class SettingsWindowViewModel : ViewModelBase
     {
+        /// <summary>
+        /// A list with all the update strategies
+        /// </summary>
         public List<PluginMetaData> List { get; }
+
+        /// <summary>
+        /// Ask before closing setting 
+        /// </summary>
         public bool AskBeforeClosing
         {
             get => askBeforeClosing;
             set => this.RaiseAndSetIfChanged(ref askBeforeClosing, value);
         }
+        /// <summary>
+        /// Private acces for ask before closing setting
+        /// </summary>
         private bool askBeforeClosing;
 
+        /// <summary>
+        /// The selected theme index
+        /// </summary>
         public int ThemeMode
         {
             get => themeMode;
             set => this.RaiseAndSetIfChanged(ref themeMode, value);
         }
+        /// <summary>
+        /// Private selected theme index
+        /// </summary>
         private int themeMode;
 
+        /// <summary>
+        /// Check for updates on start
+        /// </summary>
         public bool CheckUpdateOnStart
         {
             get => checkUpdateOnStart;
             set => this.RaiseAndSetIfChanged(ref checkUpdateOnStart, value);
         }
+        /// <summary>
+        /// Private check for updates on start
+        /// </summary>
         private bool checkUpdateOnStart;
 
+        /// <summary>
+        /// The current updater to use
+        /// </summary>
         public PluginMetaData Updater
         {
             get => updater;
             set => this.RaiseAndSetIfChanged(ref updater, value);
         }
+        /// <summary>
+        /// Private updater to use
+        /// </summary>
         private PluginMetaData updater;
-
+        
+        /// <summary>
+        /// Index of the selected updater
+        /// </summary>
         public int UpdaterIndex
         {
             get => updaterIndex;
             set => this.RaiseAndSetIfChanged(ref updaterIndex, value);
         }
+        /// <summary>
+        /// Private index of the selected updater
+        /// </summary>
         private int updaterIndex;
 
+        /// <summary>
+        /// The setting scope of this application
+        /// </summary>
         private readonly ISettingScope applicationScope;
 
+        /// <summary>
+        /// Create a new instance of this view
+        /// </summary>
+        /// <param name="view">The view for this model</param>
+        /// <param name="settingsManager">The settings manager to use</param>
+        /// <param name="pluginManager">The plugin manager to use</param>
         public SettingsWindowViewModel(ViewContainer view, ISettingsManager settingsManager, IPluginManager pluginManager)
             : base(view, settingsManager, pluginManager)
         {
@@ -71,6 +116,9 @@ namespace XmlFormatterOsIndependent.ViewModels
             LoadSettings();
         }
 
+        /// <summary>
+        /// Load all the settings
+        /// </summary>
         private void LoadSettings()
         {
             AskBeforeClosing = GetSettingsValue<bool>("AskBeforeClosing");
@@ -101,18 +149,30 @@ namespace XmlFormatterOsIndependent.ViewModels
             }
         }
 
+        /// <summary>
+        /// Get a specific settings value
+        /// </summary>
+        /// <typeparam name="T">Type of the value to get</typeparam>
+        /// <param name="name">Name of the value to get</param>
+        /// <returns>The value casted to the given type</returns>
         private T GetSettingsValue<T>(string name)
         {
             ISettingPair settingPair = applicationScope.GetSetting(name);
             return settingPair == null ? default : settingPair.GetValue<T>();
         }
 
+        /// <summary>
+        /// Close this window
+        /// </summary>
         public void CloseWindow()
         {
             ICommand command = new CloseWindowCommand();
             ExecuteCommand(command, new CloseWindowData(view.GetWindow()));
         }
 
+        /// <summary>
+        /// Save the settings and close this window
+        /// </summary>
         public void SaveAndClose()
         {
             ThemeEnum theme = ThemeMode == 0 ? ThemeEnum.Light : ThemeEnum.Dark;
