8a8226ecf7f70790b0d9407f0d64673808c56675
Simon Aberle <10531466+XanatosX@users.noreply.github.com>
2021-04-27 18:12:12 +0200

    Add version loading

--- src/XmlFormatterOsIndependent/Update/LocalVersionRecieverStrategy.cs
+++ src/XmlFormatterOsIndependent/Update/LocalVersionRecieverStrategy.cs
@@ -1,38 +1,23 @@
-﻿using PluginFramework.EventMessages;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using XmlFormatterModel.Update;
-using XmlFormatterModel.Update.Strategies;
+﻿using System.Reflection;
+using XmlFormatterModel.Update.Adapter;
 
 namespace XmlFormatterOsIndependent.Update
 {
     /// <summary>
     /// Strategy to use to get the local version
     /// </summary>
-    internal class LocalVersionRecieverStrategy : IVersionRecieverStrategy
-    {
-        /// <summary>
-        /// Was there any error in recieving the version
-        /// </summary>
-        public event EventHandler<BaseEventArgs> Error;
-
+    internal class LocalVersionRecieverStrategy : LocalResourceVersionReciever
+    { 
         /// <inheritdoc/>
-        public async Task<IRelease> GetLatestRelease()
+        protected override Assembly GetAssembly()
         {
-            return default;
+            return Assembly.GetExecutingAssembly();
         }
 
         /// <inheritdoc/>
-        public async Task<List<IRelease>> GetReleases()
+        protected override string GetResourcePath()
         {
-            return default;
-        }
-
-        /// <inheritdoc/>
-        public async Task<Version> GetVersion(IVersionConvertStrategy convertStrategy)
-        {
-            return new Version(0, 0);
+            return "XmlFormatterOsIndependent.Version.txt";
         }
     }
 }
