a325436f98f8515fd91a4b35756470930ae4b8d6
XanatosX <10531466+XanatosX@users.noreply.github.com>
2023-06-08 21:34:12 +0200

    Remove unused classes
    Rework plugin view to use DI and MVVM Toolkit

--- src/XmlFormatterOsIndependent/ViewModels/PluginManagerViewModel.cs
+++ src/XmlFormatterOsIndependent/ViewModels/PluginManagerViewModel.cs
@@ -1,12 +1,10 @@
-﻿using PluginFramework.DataContainer;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PluginFramework.DataContainer;
 using PluginFramework.Interfaces.Manager;
 using PluginFramework.Interfaces.PluginTypes;
-using ReactiveUI;
 using System.Collections.Generic;
 using System.Linq;
-using XmlFormatterModel.Setting;
-using XmlFormatterOsIndependent.Commands;
-using XmlFormatterOsIndependent.EventArg;
 using XmlFormatterOsIndependent.Models;
 
 namespace XmlFormatterOsIndependent.ViewModels
@@ -14,30 +12,18 @@ namespace XmlFormatterOsIndependent.ViewModels
     /// <summary>
     /// Window to list all the plugin data
     /// </summary>
-    public class PluginManagerViewModel : ViewModelBase
+    public partial class PluginManagerViewModel : ObservableObject
     {
         /// <summary>
         /// Is the information panel to the right visible
         /// </summary>
-        public bool PanelVisible
-        {
-            get => panelVisible;
-            private set => this.RaiseAndSetIfChanged(ref panelVisible, value);
-        }
+        [ObservableProperty]
         private bool panelVisible;
-
-        /// <summary>
-        /// The current plugin information to display
-        /// </summary>
-        public PluginInformation PluginInformation
-        {
-            get => pluginInformation;
-            private set => this.RaiseAndSetIfChanged(ref pluginInformation, value);
-        }
 
         /// <summary>
         /// Private plugin information which should be displayed
         /// </summary>
+        [ObservableProperty]
         private PluginInformation pluginInformation;
 
         /// <summary>
@@ -46,28 +32,13 @@ namespace XmlFormatterOsIndependent.ViewModels
         public List<PluginTreeViewGroup> PluginGroups { get; }
 
         /// <summary>
-        /// Command to use to open the plugin and show the information
-        /// </summary>
-        public ITriggerCommand OpenPluginCommand { get; }
-
-        /// <summary>
         /// Create a new instance of this class
         /// </summary>
         /// <param name="viewContainer">The container for the parent window and the current window</param>
         /// <param name="managerFactory">The factory to create the plugin manager</param>
-        public PluginManagerViewModel(ISettingsManager settingsManager, IPluginManager pluginManager) //ViewContainer viewContainer, 
-            : base(settingsManager, pluginManager)
+        public PluginManagerViewModel(IPluginManager pluginManager) //ViewContainer viewContainer, 
         {
             PanelVisible = false;
-            OpenPluginCommand = new GetPluginInformationCommand(pluginManager);
-            OpenPluginCommand.ContinueWith += (sender, data) =>
-            {
-                if (data is PluginInformationArg arg)
-                {
-                    PanelVisible = true;
-                    PluginInformation = arg.Information;
-                }
-            };
             PluginGroups = new List<PluginTreeViewGroup>();
             PluginTreeViewGroup formatterGroup = new PluginTreeViewGroup("Formatter");
             PluginTreeViewGroup updaterGroup = new PluginTreeViewGroup("Updater");
@@ -83,6 +54,13 @@ namespace XmlFormatterOsIndependent.ViewModels
             }
             PluginGroups.Add(formatterGroup);
             PluginGroups.Add(updaterGroup);
+        }
+
+        [RelayCommand]
+        public void OpenPlugin(PluginInformation pluginInformation)
+        {
+            PanelVisible = true;
+            PluginInformation = pluginInformation;
         }
     }
 }
