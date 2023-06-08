0343e736325840a4042e24a4e4e3a5b76640f239
XanatosX <10531466+XanatosX@users.noreply.github.com>
2023-06-08 22:34:17 +0200

    Redesign plugin information
    Add missing comments

--- src/XmlFormatterOsIndependent/ViewModels/PluginInformationViewModel.cs
+++ src/XmlFormatterOsIndependent/ViewModels/PluginInformationViewModel.cs
@@ -2,25 +2,58 @@
 using PluginFramework.DataContainer;
 
 namespace XmlFormatterOsIndependent.ViewModels;
+
+/// <summary>
+/// Model to display plugin information
+/// </summary>
 internal partial class PluginInformationViewModel : ObservableObject
 {
-    [ObservableProperty]
-    private string author;
-
+    /// <summary>
+    /// THe name of the plugin
+    /// </summary>
     [ObservableProperty]
     private string name;
 
+    /// <summary>
+    /// The author of the plugin
+    /// </summary>
+    [ObservableProperty]
+    private string author;
+
+    /// <summary>
+    /// The version of the plugin
+    /// </summary>
     [ObservableProperty]
     private string version;
 
+    /// <summary>
+    /// The description of the plugin
+    /// </summary>
     [ObservableProperty]
     private string description;
 
+    /// <summary>
+    /// Create a new instance of this class
+    /// </summary>
+    /// <param name="pluginInformation">The plugin information to use</param>
     public PluginInformationViewModel(PluginInformation pluginInformation)
     {
-        Author = pluginInformation.Author;
         Name = pluginInformation.Name;
-        Version = pluginInformation.Version.ToString();
+        Author = pluginInformation.Author;
+        string major = ConvertToVersion(pluginInformation.Version.Major);
+        string minor = ConvertToVersion(pluginInformation.Version.Minor);
+        string build = ConvertToVersion(pluginInformation.Version.Build);
+        Version = $"{major}.{minor}.{build}";
         Description = pluginInformation.Description;
+    }
+
+    /// <summary>
+    /// Method to convert a given number to a correct version number
+    /// </summary>
+    /// <param name="number">The number to convert</param>
+    /// <returns>A version number between 0 and max int as string</returns>
+    private string ConvertToVersion(int number)
+    {
+        return number < 0 ? "0" : number.ToString();
     }
 }
