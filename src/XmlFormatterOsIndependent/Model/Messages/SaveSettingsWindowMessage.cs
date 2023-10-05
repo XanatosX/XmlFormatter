703ec4e20e07d653229ccb2e59ca53fcb7556d28
XanatosX <10531466+XanatosX@users.noreply.github.com>
2023-10-05 22:27:32 +0200

    Add missing doc blocks

--- src/XmlFormatterOsIndependent/Model/Messages/SaveSettingsWindowMessage.cs
+++ src/XmlFormatterOsIndependent/Model/Messages/SaveSettingsWindowMessage.cs
@@ -1,6 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.Messaging.Messages;
 
 namespace XmlFormatterOsIndependent.Model.Messages;
+
+/// <summary>
+/// Message to trigger a setting save
+/// </summary>
 internal class SaveSettingsWindowMessage : ValueChangedMessage<bool>
 {
     public SaveSettingsWindowMessage(bool value) : base(value)
--- src/XmlFormatterOsIndependent/Model/Messages/SettingsImportedMessage.cs
+++ src/XmlFormatterOsIndependent/Model/Messages/SettingsImportedMessage.cs
@@ -1,8 +1,10 @@
 using CommunityToolkit.Mvvm.Messaging.Messages;
-using XmlFormatterOsIndependent.Model;
 
 namespace XmlFormatterOsIndependent.Model.Messages;
 
+/// <summary>
+/// Message if a new setting version was imported
+/// </summary>
 internal class SettingsImportedMessage : ValueChangedMessage<ApplicationSettings>
 {
     internal SettingsImportedMessage(ApplicationSettings value) : base(value)
