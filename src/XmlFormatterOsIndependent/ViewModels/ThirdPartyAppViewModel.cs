b961f84f43b490bca518ed9809fa676a4d0fd991
XanatosX <10531466+XanatosX@users.noreply.github.com>
2023-10-08 22:07:51 +0200

    Add missing doc blocks

--- src/XmlFormatterOsIndependent/ViewModels/ThirdPartyAppViewModel.cs
+++ src/XmlFormatterOsIndependent/ViewModels/ThirdPartyAppViewModel.cs
@@ -3,24 +3,45 @@ using XmlFormatterOsIndependent.Model;
 
 namespace XmlFormatterOsIndependent.ViewModels;
 
+/// <summary>
+/// View model for the an single third party app
+/// </summary>
 internal partial class ThirdPartyAppViewModel : ObservableObject
 {
+    /// <summary>
+    /// The name of the app
+    /// </summary>
     [ObservableProperty]
     private string name;
 
+    /// <summary>
+    /// The version of the app
+    /// </summary>
     [ObservableProperty]
     private string version;
 
+    /// <summary>
+    /// The license type of the version
+    /// </summary>
     [ObservableProperty]
     private string license;
 
+    /// <summary>
+    /// The license url of the app
+    /// </summary>
     [ObservableProperty]
     private string? licenseUrl;
 
-    
+    /// <summary>
+    /// The url to the third party app
+    /// </summary>
     [ObservableProperty]
     private string url;
 
+    /// <summary>
+    /// Create a new instance of the view model
+    /// </summary>
+    /// <param name="appData">The app data to use</param>
     public ThirdPartyAppViewModel(ThirdPartyApp appData)
     {
         Name = appData.Name;
